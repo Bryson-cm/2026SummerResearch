28405e1dfe232c47d4101fab8de7797593c65da9
Bryson-cm <brysoncm@icloud.com>
2026-06-16 13:52:33 -0400

    Begun the complied electron trajectory Jupiter notebook. This notebook will allow you to run all python programs from one place.

---
+++ input/ElectronTrajectory_input.py
@@ -0,0 +1,12 @@
+#This line will create betatron_example.py input file in the subfolder 'input'.
+
+#Intial Electron Paramters 
+y0 = 0.1 #initial transverse position in y0/rb
+dy = 0.01 #initial beam width in dy/rb
+
+#Animations and plotting parameters
+L = 500 #length of the plot in microns
+N = 1000 #number of points in the plot
+
+
+
